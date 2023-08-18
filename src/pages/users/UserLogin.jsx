e747ce1daa4784ff0ad775a607a0e471cf953bd1
TEJASBANGERA97 <tejas.bangera12@gmail.com>
2023-08-18 20:24:12 +0530

    Completed user signup and login features

--- src/pages/users/UserLogin.jsx
+++ src/pages/users/UserLogin.jsx
@@ -1,16 +1,43 @@
-import { Link } from "react-router-dom";
-import Form from "../../components/AuthForm";
+import { Link, useNavigate } from "react-router-dom";
+import AuthForm from "../../components/AuthForm";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { login, setUsername } from "../../store/user/userSlice";
 
 const UserLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("Form submitted!");
+    axios
+      .post("http://localhost:3000/users/login", null, {
+        headers: {
+          username: email,
+          password,
+        },
+      })
+      .then((response) => {
+        const { token } = response.data;
+        localStorage.setItem("user-token", "Bearer " + token);
+        dispatch(setUsername(email));
+        dispatch(login());
+        navigate("/");
+      })
+      .catch((error) => console.log(error));
   }
 
   return (
     <div className="bg-white w-full max-w-md flex flex-col p-10 mt-10 self-center">
       <h1>Login</h1>
-      <Form handleSubmit={handleSubmit} />
+      <AuthForm
+        {...{ email, setEmail, password, setPassword }}
+        handleSubmit={handleSubmit}
+      />
       <Link
         to={"/users/signup"}
         className="text-xs underline self-start mt-3 text-slate-500"
--- src/pages/users/UserSignUp.jsx
+++ src/pages/users/UserSignUp.jsx
@@ -1,16 +1,40 @@
-import { Link } from "react-router-dom";
-import Form from "../../components/AuthForm";
+import { Link, useNavigate } from "react-router-dom";
+import AuthForm from "../../components/AuthForm";
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { login, setUsername } from "../../store/user/userSlice";
 
 const UserSignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("Form submitted!");
+    axios
+      .post("http://localhost:3000/users/signup", {
+        username: email,
+        password,
+      })
+      .then((response) => {
+        const { token } = response.data;
+        localStorage.setItem("user-token", "Bearer " + token);
+        dispatch(setUsername(email));
+        dispatch(login());
+        navigate("/");
+      })
+      .catch((error) => console.log(error));
   }
 
   return (
     <div className="bg-white w-full max-w-md flex flex-col self-center p-10 mt-10">
       <h1>Sign Up</h1>
-      <Form handleSubmit={handleSubmit} />
+      <AuthForm
+        {...{ email, setEmail, password, setPassword }}
+        handleSubmit={handleSubmit}
+      />
       <Link
         to={"/users/login"}
         className="text-xs underline self-start mt-3 text-slate-500"
