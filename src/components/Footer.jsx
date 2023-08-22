736cdfd297b61be73973bf7001c102d2047235c4
TEJASBANGERA97 <tejas.bangera12@gmail.com>
2023-08-23 00:21:54 +0530

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,20 @@
+import GitHub from "./GitHub";
+import LinkedIn from "./LinkedIn";
+import Mail from "./Mail";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex items-center p-2">
+      <GitHub />
+      <p className="mx-auto text-sm italic">
+        Made by <span className="font-bold">Tejas Bangera</span>
+      </p>
+
+      <div className="flex items-center gap-x-1">
+        <LinkedIn />
+        <Mail />
+      </div>
+    </footer>
+  );
 };
 export default Footer;
